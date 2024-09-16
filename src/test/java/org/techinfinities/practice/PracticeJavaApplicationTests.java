58e94bf32753db960a859d8415f3f65285fc363f
Pratik Patel <patelpratik99979@gmail.com>
2024-09-16 22:42:47 +0530

    Commit

---
+++ src/test/java/org/techinfinities/practice/PracticeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.techinfinities.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
